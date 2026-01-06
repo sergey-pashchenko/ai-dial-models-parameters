a5ce595dcd7c5516ac80758aa9360b2af9283efa
Sergey Pashchenko <sepanko@mumdei.com>
2026-01-06 20:35:55 +0400

    task 3 done

--- task/3-task-temperature.py
+++ task/3-task-temperature.py
@@ -6,9 +6,17 @@ from task.app.main import run
 #  User massage: Describe the sound that the color purple makes when it's angry
 
 run(
-    deployment_name="gpt-4o",
+    # deployment_name="gpt-4o",
+    # deployment_name="claude-sonnet-4@20250514",
+    deployment_name="gemini-2.5-pro",
     print_only_content=True,
+    # print_only_content=False,
     # TODO:
     #  Use `temperature` parameter with value in range from 0.0 to 1.0!
     #  (Optional) Use `temperature` parameter with value 2.1 and check what happens
+    # temperature=0.0,
+    # temperature=1.5,
+    temperature=1.7,
+    # temperature=1.75,
+    # n=3,
 )
