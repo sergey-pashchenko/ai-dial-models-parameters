b8e8c8d594a11da34e103e6979e73a19f12f6cc8
Sergey Pashchenko <sepanko@mumdei.com>
2026-01-06 20:04:58 +0400

    task 1 done

--- task/1-task-models.py
+++ task/1-task-models.py
@@ -11,11 +11,16 @@ from task.app.main import run
 # - claude-3-7-sonnet@20250219
 # - gemini-2.5-pro
 
+# The main goal of this task is to explore the functional capabilities of DIAL to be able to work with different
+# LLMs through unified API
+
 run(
-    deployment_name="INSERT_DEPLOYMENT_NAME",
+    # deployment_name="gpt-4o",
+    # # deployment_name="anthropic.claude-3-7-sonnet-20250219-v1:0",
+    # # deployment_name="anthropic.claude-sonnet-4-5-20250929-v1:0",
+    # # deployment_name="anthropic.claude-sonnet-4-5-20250929-v1:0",
+    deployment_name="claude-sonnet-4@20250514",
+    # deployment_name="gemini-2.5-pro",
     print_request=False,  # Switch to False if you do not want to see the request in console
     print_only_content=False,  # Switch to True if you want to see only content from response
 )
-
-# The main goal of this task is to explore the functional capabilities of DIAL to be able to work with different
-# LLMs through unified API
